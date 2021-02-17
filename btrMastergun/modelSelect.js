07f0226782d592e2e4fbd5f07018ebe3a9b6b171
MasterGunPro <46889642+MasterGunPro@users.noreply.github.com>
2021-02-17 11:29:52 +0300

    Add trim

--- btrMastergun/modelSelect.js
+++ btrMastergun/modelSelect.js
@@ -10,7 +10,7 @@ $(document).ready(
 			var rifle = getProductBySkuPrefix('R');
 			
 			if(rifle.length > 0){		
-				$('a:contains("Ложе")').attr('href', buttUrl+rifle[0].sku);
+				$('a:contains("Ложе")').attr('href', buttUrl+rifle[0].sku.trim());
 			}
 			else{
 				$('.t758__wrapper').hide();
