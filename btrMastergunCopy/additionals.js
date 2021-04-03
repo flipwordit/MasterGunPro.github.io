b74d5c7d8c3e66c6f183902893e82f711e487e52
Mikhail Tatarkov <tmu87@mail.ru>
2021-04-03 13:47:45 +0300

    Перенес конфигурацию цен в начало скрипта

--- btrMastergunCopy/additionals.js
+++ btrMastergunCopy/additionals.js
@@ -1,3 +1,96 @@
+var configuration = [
+	{
+		products: [
+			//Общие аксессуары
+			{ sku: "D_108", price: "1500" },
+			{ sku: "D_102", price: "4500" },
+			{ sku: "D_109", price: "1200" },
+			{ sku: "D_105", price: "1800" },
+			{ sku: "D_111", price: "10700" },
+			{ sku: "D_112", price: "3500" },
+			{ sku: "D_113", price: "5500" },
+			{ sku: "D_101", price: "7000" },
+			{ sku: "D_103", price: "2700" },
+			{ sku: "D_104", price: "2000" },
+			{ sku: "D_106", price: "900" },
+			{ sku: "D_107", price: "1200" },
+			{ sku: "D_110", price: "3500" },
+			{ sku: "D_001", price: "2100" },
+			{ sku: "D_002", price: "3200" },
+
+			//Аксессуары отображающиеся по условию
+			{ sku: "D_114", visible: false }, //Шасси
+			{ sku: "D_115", visible: false }, //Комплект кожаных встравок
+			{ sku: "D_315", visible: false }, //Шильдик на гребень
+			{ sku: "D_820", visible: false }, //Магазин стандарта AICS 5 местный
+			{ sku: "D_821", visible: false }, //Магазин стандарта AICS 8 местный
+			{ sku: "D_822", visible: false }, //Магазин стандарта AICs 10 местный
+			{ sku: "D_823", visible: false }, //Магазин стандарта AICs 12 местный
+		]
+	},
+	{
+		rifle: "R_BR93",
+		products: [
+			{
+				sku: "D_114", //Шасси
+				visible: true,
+				price: 75000
+			},
+		]
+	},
+	{
+		rifle: "R_BR8",
+		products: [
+			{
+				sku: "D_114", //Шасси
+				visible: true,
+				price: 65000
+			},
+		]
+	},
+	{
+		model: "MG-03",
+		products: [
+			{
+				sku: "D_115", //Комплект кожаных встравок
+				visible: true,
+				price: 6000
+			},
+			{
+				sku: "D_315", //Шильдик на гребень
+				visible: true,
+				price: 2700
+			},
+		]
+	},
+	{
+		model: "MG-08",
+		products: [
+			{
+				sku: "D_820", //Магазин стандарта AICS 5 местный
+				visible: true,
+				price: 6500
+			},
+			{
+				sku: "D_821", //Магазин стандарта AICS 8 местный
+				visible: true,
+				price: 6800
+			},
+			{
+				sku: "D_822", //Магазин стандарта AICs 10 местный
+				visible: true,
+				price: 7200
+			},
+			{
+				sku: "D_823", //Магазин стандарта AICs 12 местный
+				visible: true,
+				price: 7500
+			},
+		]
+	},
+];
+
+
 var buttUrl = baseUrl + '/butts/';
 
 $(document).ready(
@@ -17,98 +110,6 @@ $(document).ready(
 				}
 
 				var model = getProductBySkuPrefix('MG');
-
-				var configuration = [
-					{
-						products: [
-							//Общие аксессуары
-							{ sku: "D_108", price: "1500" },
-							{ sku: "D_102", price: "4500" },
-							{ sku: "D_109", price: "1200" },
-							{ sku: "D_105", price: "1800" },
-							{ sku: "D_111", price: "10700" },
-							{ sku: "D_112", price: "3500" },
-							{ sku: "D_113", price: "5500" },
-							{ sku: "D_101", price: "7000" },
-							{ sku: "D_103", price: "2700" },
-							{ sku: "D_104", price: "2000" },
-							{ sku: "D_106", price: "900" },
-							{ sku: "D_107", price: "1200" },
-							{ sku: "D_110", price: "3500" },
-							{ sku: "D_001", price: "2100" },
-							{ sku: "D_002", price: "3200" },
-							
-							//Аксессуары отображающиеся по условию
-							{ sku: "D_114", visible: false }, //Шасси
-							{ sku: "D_115", visible: false }, //Комплект кожаных встравок
-							{ sku: "D_315", visible: false }, //Шильдик на гребень
-							{ sku: "D_820", visible: false }, //Магазин стандарта AICS 5 местный
-							{ sku: "D_821", visible: false }, //Магазин стандарта AICS 8 местный
-							{ sku: "D_822", visible: false }, //Магазин стандарта AICs 10 местный
-							{ sku: "D_823", visible: false }, //Магазин стандарта AICs 12 местный
-						]
-					},
-					{
-						rifle: "R_BR93",
-						products: [
-							{
-								sku: "D_114", //Шасси
-								visible: true,
-								price: 75000
-							},
-						]
-					},
-					{
-						rifle: "R_BR8",
-						products: [
-							{
-								sku: "D_114", //Шасси
-								visible: true,
-								price: 65000
-							},
-						]
-					},
-					{
-						model: "MG-03",
-						products: [
-							{
-								sku: "D_115", //Комплект кожаных встравок
-								visible: true,
-								price: 6000
-							},
-							{
-								sku: "D_315", //Шильдик на гребень
-								visible: true,
-								price: 2700
-							},
-						]
-					},
-					{
-						model: "MG-08",
-						products: [
-							{
-								sku: "D_820", //Магазин стандарта AICS 5 местный
-								visible: true,
-								price: 6500
-							},
-							{
-								sku: "D_821", //Магазин стандарта AICS 8 местный
-								visible: true,
-								price: 6800
-							},
-							{
-								sku: "D_822", //Магазин стандарта AICs 10 местный
-								visible: true,
-								price: 7200
-							},
-							{
-								sku: "D_823", //Магазин стандарта AICs 12 местный
-								visible: true,
-								price: 7500
-							},
-						]
-					},
-				];
 
 				var rifleSku = rifle[0].sku.trim();
 				var modelSku = model[0].sku.trim();
