01db230f3f0a0f49fb6934a3980a8779585d8b0e
MasterGunPro <46889642+MasterGunPro@users.noreply.github.com>
2021-06-20 15:43:23 +0300

    Меняю ссылку на ложе только для хлебных крошек

--- btrMastergunCopy/color.js
+++ btrMastergunCopy/color.js
@@ -7,7 +7,7 @@ $(document).ready(
 			var rifle = getProductBySkuPrefix('R');
 			
 			if(rifle.length > 0){	 
-				$('a:contains("Ложе")').attr('href', buttUrl+rifle[0].sku.trim());
+				$('.t758__list a:contains("Ложе")').attr('href', buttUrl+rifle[0].sku.trim());
 			}
 			else{
 					$('.t758__wrapper').hide();
